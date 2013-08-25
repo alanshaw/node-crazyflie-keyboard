f41a97773e9d254c8771a49992a7b2d40fce0b8b
Alan Shaw <alan@freestyle-developments.co.uk>
2013-08-25 14:10:00 +0100

    Thrust fun

--- index.js
+++ index.js
@@ -34,6 +34,8 @@ stdin.resume()
 // i don't want binary, do you?
 stdin.setEncoding("utf8")
 
+var thrust = 40000
+
 // on any data into stdin
 stdin.on("data", function (key) {
   // ctrl-c (end of text)
@@ -49,7 +51,9 @@ stdin.on("data", function (key) {
       console.log("take off...")
       copter.takeoff().then(function () {
         copter.hover()
-      }).then(function () {
+        copter.setPitch(0)
+        copter.setRoll(0)
+        copter.setThrust(thrust)
         ready = true
       })
       break;
@@ -66,23 +70,65 @@ stdin.on("data", function (key) {
       break;
     case "\u001b[A": // Up
       console.log("up...")
-      copter.setPitch(25)
-      copter.setRoll(0)
+      
+      copter.setPitch(15)
+      
+      ready = false
+      setTimeout(function () {
+        copter.setPitch(0)
+        ready = true
+      }, 250)
+      
       break;
     case "\u001b[B": // Down
       console.log("down...")
-      copter.setPitch(-25)
-      copter.setRoll(0)
+      
+      copter.setPitch(-15)
+      
+      ready = false
+      setTimeout(function () {
+        copter.setPitch(0)
+        ready = true
+      }, 250)
+      
       break;
     case "\u001b[D": // Left
       console.log("left...")
-      copter.setPitch(0)
-      copter.setRoll(25)
+      
+      copter.setRoll(15)
+      
+      ready = false
+      setTimeout(function () {
+        copter.setRoll(0)
+        ready = true
+      }, 250)
+      
       break;
     case "\u001b[C": // Right
       console.log("right...")
-      copter.setPitch(0)
-      copter.setRoll(-25)
+      
+      copter.setRoll(-15)
+      
+      ready = false
+      setTimeout(function () {
+        copter.setRoll(0)
+        ready = true
+      }, 250)
+      
+      break;
+    case "a":
+      console.log("thrust up...")
+      
+      thrust = thrust + 1000
+      copter.setThrust(thrust)
+      
+      break;
+    case "z":
+      console.log("thrust down...")
+      
+      thrust = thrust - 1000
+      copter.setThrust(thrust)
+      
       break;
     default: console.log("Unkown command %s", key)
   }
